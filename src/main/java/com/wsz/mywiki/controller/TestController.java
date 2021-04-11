525c3bfd0ee0c2058f312918b3d9d8aae6c0ed83
Duncan Edwards <305185235@qq.com>
2021-04-11 15:45:05 +0800

    2-8 增加自定义配置项

--- src/main/java/com/wsz/mywiki/controller/TestController.java
+++ src/main/java/com/wsz/mywiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wsz.mywiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 //返回字符串
 @RestController
 public class TestController {
+    @Value("${test.one:one}")
+    private String one;
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello"+one;
     }
 
     @PostMapping("/hello/post")
