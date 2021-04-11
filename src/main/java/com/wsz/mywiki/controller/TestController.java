c0d03769544701580830540f8abf42ff838080dd
Duncan Edwards <305185235@qq.com>
2021-04-11 15:29:19 +0800

    2-7 用Http Client测试hello接口

--- src/main/java/com/wsz/mywiki/controller/TestController.java
+++ src/main/java/com/wsz/mywiki/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.wsz.mywiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //返回字符串
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/hello/post")
+    public String hellopost(String name) {
+        return "hello post "+name;
     }
 }
