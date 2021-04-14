e3fbd8c5da5996f5ee8f3b1c0a29bd8d2fe14487
Duncan Edwards <305185235@qq.com>
2021-04-14 15:20:47 +0800

    6-11 为保存接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -78,7 +78,7 @@ export default defineComponent({
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 3,
+      pageSize: 10,
       total: 0
     });
     const loading = ref(false);
@@ -166,16 +166,18 @@ export default defineComponent({
 
       axios.post("/ebook/save",ebook.value).then((res)=>{
         const data=res.data;
+        modalLoading.value = false;
         if (data.success) {
           modalVisible.value = false;
-          modalLoading.value = false;
           //重新加载列表
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize
           });
         }
-
+        else {
+          message.error(data.message)
+        }
       })
 
     };
