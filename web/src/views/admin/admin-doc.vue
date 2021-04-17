f4771e3d9c4bec177da913a275ac59e731d06029
Duncan Edwards <305185235@qq.com>
2021-04-17 19:58:07 +0800

    8-6 删除某一个文档时，将其下所有文档全部删除

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -218,6 +218,38 @@ export default defineComponent({
       }
     };
 
+    const ids:Array<string>=[];
+    /**
+     * 查找整根树枝
+     */
+    const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标id放入结果集
+          ids.push(id);
+
+          // 遍历所有子节点
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              getDeleteIds(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            getDeleteIds(children, id);
+          }
+        }
+      }
+    };
+
     /**
      * 编辑
      */
@@ -247,7 +279,10 @@ export default defineComponent({
     };
 
     const handleDelete = (id:number) => {
-      axios.delete("/doc/delete/"+id).then((res)=>{
+      console.log(level1,id);
+      getDeleteIds(level1.value,id);
+      console.log(ids);
+      axios.delete("/doc/delete/"+ids.join(",")).then((res)=>{
         const data=res.data;
         if (data.success) {
           //重新加载列表
