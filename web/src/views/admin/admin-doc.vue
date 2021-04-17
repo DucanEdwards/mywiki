494e1abd598d593c6de4e5da325dbd3ec8657b46
Duncan Edwards <305185235@qq.com>
2021-04-17 20:28:24 +0800

    8-7 集成富文本框，在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -74,6 +74,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -85,6 +88,7 @@ import { message,Modal  } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
 import { ExclamationCircleOutlined } from '@ant-design/icons-vue';
+import E from "wangeditor"
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -170,6 +174,9 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const editor = new E("#content")
+
+
     const handleModalOk = () => {
       modalLoading.value = true;
 
@@ -265,6 +272,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100)
     };
 
     /**
@@ -279,6 +289,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100)
     };
 
     const handleDelete = (id:number) => {
