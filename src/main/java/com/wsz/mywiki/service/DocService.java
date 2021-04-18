836be33467251860b9446ad3aa011fc21325ab60
Duncan Edwards <305185235@qq.com>
2021-04-18 20:17:59 +0800

    8-12 修复Bug，文档内容表没数据会出现空指针异常

--- src/main/java/com/wsz/mywiki/service/DocService.java
+++ src/main/java/com/wsz/mywiki/service/DocService.java
@@ -109,7 +109,13 @@ public class DocService {
 
     public String findContent(Long id) {
         Content content=contentMapper.selectByPrimaryKey(id);
-        return content.getContent();
+        if(ObjectUtils.isEmpty(content)) {
+            return "";
+        }
+        else {
+            return content.getContent();
+        }
+
     }
 
 
