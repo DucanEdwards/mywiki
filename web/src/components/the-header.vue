1394a4aad269fd76c2dd6615e63bd49880c88f35
Duncan Edwards <305185235@qq.com>
2021-04-19 18:07:47 +0800

    9-11 增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,6 +21,16 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+      >
+        <a class="login-menu" v-show="user.id">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
       <a class="login-menu" v-show="user.id">
         <span>您好：{{user.name}}</span>
       </a>
@@ -92,13 +102,28 @@ export default defineComponent({
       });
     };
 
+    // 登录
+    const logout = () => {
+      console.log("退出登录");
+      axios.get('/user/logout/'+ user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser",{});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     return {
       loginModalVisible,
       loginModalLoading,
       showLoginModal,
       loginUser,
       login,
-      user
+      user,
+      logout
     }
   }
 });
@@ -108,5 +133,6 @@ export default defineComponent({
 .login-menu {
   float: right;
   color: white;
+  padding-left: 10px;
 }
 </style>
