001c49cf22c1eeb7e859fefbb5acab63a8a03da4
Duncan Edwards <305185235@qq.com>
2021-04-21 18:09:14 +0800

    10-8 集成websocket

--- web/src/util/tool.ts
+++ web/src/util/tool.ts
@@ -21,7 +21,7 @@ export class Tool {
      * 对象复制
      * @param obj
      */
-    public static copy (obj: object) {
+    public static copy (obj: any) {
         if (Tool.isNotEmpty(obj)) {
             return JSON.parse(JSON.stringify(obj));
         }
@@ -51,5 +51,23 @@ export class Tool {
             }
         }
         return result;
+    }
+
+    /**
+     * 随机生成[len]长度的[radix]进制数
+     * @param len
+     * @param radix 默认62
+     * @returns {string}
+     */
+    public static uuid (len: number, radix = 62) {
+        const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+        const uuid = [];
+        radix = radix || chars.length;
+
+        for (let i = 0; i < len; i++) {
+            uuid[i] = chars[0 | Math.random() * radix];
+        }
+
+        return uuid.join('');
     }
 }
