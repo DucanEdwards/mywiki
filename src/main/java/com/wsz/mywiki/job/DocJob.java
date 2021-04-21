19f070a370424641d8d75c19604a665a81fc70ec
Duncan Edwards <305185235@qq.com>
2021-04-21 15:23:48 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/wsz/mywiki/job/DocJob.java
+++ src/main/java/com/wsz/mywiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.wsz.mywiki.job;
 
 import com.wsz.mywiki.service.DocService;
+import com.wsz.mywiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +17,16 @@ public class DocJob {
 
     @Resource
     private DocService docService;
+
+    @Resource
+    private SnowFlake snowFlake;
     /**
      * 每30秒更新电子数信息
      */
     @Scheduled(cron = "1/5 * * * * ?")
     public void cron() throws InterruptedException {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档数据开始");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
