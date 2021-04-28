f6d4696a94caf5321aa1dd02f4416fce61729cb8
Duncan Edwards <305185235@qq.com>
2021-04-28 13:21:49 +0800

    11-6 将欢迎文字提取成组件

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -25,7 +25,7 @@
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
         <div class="welcome" v-show="isShowWelcome">
-          <h1>欢迎使用mywiki</h1>
+          <the-welcome></the-welcome>
         </div>
         <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
                 :data-source="ebooks">
@@ -65,6 +65,7 @@ import { defineComponent,onMounted ,ref,reactive,toRef} from 'vue';
 import axios from "axios";
 import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue'
 
 const listData: Record<string, string>[] = [];
 
@@ -82,6 +83,9 @@ const listData: Record<string, string>[] = [];
 
 export default defineComponent({
   name: 'Home',
+  components:{
+    TheWelcome
+  },
   setup() {
     const ebooks=ref();
     // const ebooks1=reactive({books:[]})
